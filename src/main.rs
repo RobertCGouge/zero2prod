8dec26dacd0e9053643e9f5faffc7fdf2b1d9ede
Gouge, Robert <robert.gouge@cgi.com>
2025-05-06 10:05:11 -0500

    refactor complete

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
- use zero2prod::run;
-
+use std::net::TcpListener;
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run().await
+    // Bubble up the io::Error if we failed to bind the address
+    // Otherwise call .await on our Server
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Unable to bind port 8000");
+    run(listener)?.await
 }
